9388fce1f74f50595dd6b6a9fb8c585c561b28c4
shubham <mynameshubham@gmail.com>
2017-02-09 18:58:32 +0530

    only parsing csv and debugging data is left

--- js/chartApp.js
+++ js/chartApp.js
@@ -1,18 +1,31 @@
 var app = angular.module('SCChartApp', ['ng-fusioncharts']);
     app.controller('MyController', function($scope) {
-        /*
-        $scope.events = {
-            dataplotclick: function(ev, props) {
-                $scope.$apply(function() {
-                    //$scope.colorValue = "background-color:" + props.categoryLabel + ";";
-                });
-            }
+        $scope.showContent = function($fileContent){
+            $scope.content = $fileContent;
         };
-        */
-        {
-            $scope.test = function(text) {
-                alert(text);
-            }
-            $scope.dataSource = {}
+        $scope.test = function(text) {
+            alert(text);
         }
+});
+
+app.directive('onReadFile', function ($parse) {
+	return {
+		restrict: 'A',
+		scope: false,
+		link: function(scope, element, attrs) {
+            var fn = $parse(attrs.onReadFile);
+
+			element.on('change', function(onChangeEvent) {
+				var reader = new FileReader();
+
+				reader.onload = function(onLoadEvent) {
+					scope.$apply(function() {
+						fn(scope, {$fileContent:onLoadEvent.target.result});
+					});
+				};
+
+				reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
+			});
+		}
+	};
 });
