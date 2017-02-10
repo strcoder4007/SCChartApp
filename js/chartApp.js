5b31b5f7faa9201bd76910087884ec5b7befd5b3
shubham <mynameshubham@gmail.com>
2017-02-10 18:30:03 +0530

    modified sample JSON files

--- js/chartApp.js
+++ js/chartApp.js
@@ -7,29 +7,19 @@ var app = angular.module('SCChartApp', ['ng-fusioncharts']);
             console.log(text);
         };
         $scope.updateChart = function(data, dataType){
-            $("#chartContainer").updateFusionCharts({type:'line', dataSource: data, dataFormat: dataType, width: "100%", height: "550"});
+            $("#chartContainer").updateFusionCharts({type:'line', dataSource: data, dataFormat: dataType, width: "100%", height: "540"});
         };
         $scope.updateDestChart = function(dest, dataType){
-            $("#chartContainer").updateFusionCharts({type:'line', dataSource: dest, dataFormat: dataType, width: "100%", height: "550"});
+            $("#chartContainer").updateFusionCharts({type:'line', dataSource: dest, dataFormat: dataType, width: "100%", height: "540"});
         };
     });
 
-    /*
-    app.controller('myCtrl', function ($scope, $http) {
-        $scope.listOfCustomers = null;
-        $http.get('http://codehour.online/sachin.json')
-            .success(function (data) {
-                console.log(data);
-                $scope.listOfCustomers = data.alldata;
-            })
-    });
-    */
-
 app.controller('myCtrl', function ($scope, $http) {
-    $scope.listOfCustomers = null;
-    $http.get('http://www.iNorthwind.com/Service1.svc/getAllCustomers')
+    $scope.stats = null;
+    $http.get('http://localhost/angular-charts/data2.json')
         .success(function (data) {
-            $scope.listOfCustomers = data.GetAllCustomersResult;
+            var stats = data.alldata;
+            var items = ['opposition', 'stadium', 'goals', 'age', 'country'];
         })
 });
 
