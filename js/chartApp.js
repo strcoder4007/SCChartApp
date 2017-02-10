d6462a0c27834e69ec7b33fc416bdb595f07d121
shubham <mynameshubham@gmail.com>
2017-02-10 16:58:09 +0530

    major changes

--- js/chartApp.js
+++ js/chartApp.js
@@ -4,9 +4,35 @@ var app = angular.module('SCChartApp', ['ng-fusioncharts']);
             $scope.content = $fileContent;
         };
         $scope.test = function(text) {
-            alert(text);
-        }
+            console.log(text);
+        };
+        $scope.updateChart = function(data, dataType){
+            $("#chartContainer").updateFusionCharts({type:'line', dataSource: 'newdata.json', dataFormat: dataType, width: "100%", height: "550"});
+        };
+        $scope.updateDestChart = function(dest, dataType){
+            $("#chartContainer").updateFusionCharts({type:'line', dataSource: dest, dataFormat: dataType, width: "100%", height: "550"});
+        };
+    });
+
+    app.controller('myCtrl', function ($scope, $http) {
+        $scope.listOfCustomers = null;
+        $http.get('http://codehour.online/sachin.json')
+            .success(function (data) {
+                console.log(data);
+                $scope.listOfCustomers = data.alldata;
+            })
+    });
+
+    /*
+app.controller('myCtrl', function ($scope, $http) {
+    $scope.listOfCustomers = null;
+    $http.get('http://www.iNorthwind.com/Service1.svc/getAllCustomers')
+        .success(function (data) {
+            console.log(data);
+            $scope.listOfCustomers = data.GetAllCustomersResult;
+        })
 });
+*/
 
 app.directive('onReadFile', function ($parse) {
 	return {
