cbba36203bdfd69398d04dc95c588deac256e8a5
shubham <mynameshubham@gmail.com>
2017-02-10 17:20:34 +0530

    all imports working

--- js/chartApp.js
+++ js/chartApp.js
@@ -7,13 +7,14 @@ var app = angular.module('SCChartApp', ['ng-fusioncharts']);
             console.log(text);
         };
         $scope.updateChart = function(data, dataType){
-            $("#chartContainer").updateFusionCharts({type:'line', dataSource: 'newdata.json', dataFormat: dataType, width: "100%", height: "550"});
+            $("#chartContainer").updateFusionCharts({type:'line', dataSource: data, dataFormat: dataType, width: "100%", height: "550"});
         };
         $scope.updateDestChart = function(dest, dataType){
             $("#chartContainer").updateFusionCharts({type:'line', dataSource: dest, dataFormat: dataType, width: "100%", height: "550"});
         };
     });
 
+    /*
     app.controller('myCtrl', function ($scope, $http) {
         $scope.listOfCustomers = null;
         $http.get('http://codehour.online/sachin.json')
@@ -22,17 +23,15 @@ var app = angular.module('SCChartApp', ['ng-fusioncharts']);
                 $scope.listOfCustomers = data.alldata;
             })
     });
+    */
 
-    /*
 app.controller('myCtrl', function ($scope, $http) {
     $scope.listOfCustomers = null;
     $http.get('http://www.iNorthwind.com/Service1.svc/getAllCustomers')
         .success(function (data) {
-            console.log(data);
             $scope.listOfCustomers = data.GetAllCustomersResult;
         })
 });
-*/
 
 app.directive('onReadFile', function ($parse) {
 	return {
@@ -40,16 +39,13 @@ app.directive('onReadFile', function ($parse) {
 		scope: false,
 		link: function(scope, element, attrs) {
             var fn = $parse(attrs.onReadFile);
-
 			element.on('change', function(onChangeEvent) {
 				var reader = new FileReader();
-
 				reader.onload = function(onLoadEvent) {
 					scope.$apply(function() {
 						fn(scope, {$fileContent:onLoadEvent.target.result});
 					});
 				};
-
 				reader.readAsText((onChangeEvent.srcElement || onChangeEvent.target).files[0]);
 			});
 		}
